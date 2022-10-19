cf3e36c5fe107448ee263f9061b2cca28a9b419b
leiyannan <13699223155@163.com>
2022-10-19 10:20:52 +0800

    git push

--- middleware/handler.py
+++ middleware/handler.py
@@ -3,6 +3,8 @@ import json
 import os
 import random
 import time
+from datetime import date
+
 
 import requests
 import rsa
@@ -62,110 +64,129 @@ class Handler():
 
 
 
-    @property
-    def token_test1(self):
-        return login_test1()["token"]
+    # @property
+    # def token_test1(self):
+    #     return login_test1()["token"]
+    #
+    # @property
+    # def user_id_test1(self):
+    #     return login_test1()["user_id"]
+    #
+    # @property
+    # def token_test2(self):
+    #     return login_test2()["token"]
+    #
+    # @property
+    # def user_id_test2(self):
+    #     return login_test2()["user_id"]
+
 
     @property
-    def user_id_test1(self):
-        return login_test1()["user_id"]
+    def resourceId01(self):
+        # test1本地资源ID
+        return add_resource01()["resourceId01"]
 
     @property
-    def token_test2(self):
-        return login_test2()["token"]
+    def resourceFusionId01(self):
+        # test1联邦资源ID
+        return add_resource01()["resourceFusionId01"]
 
     @property
-    def user_id_test2(self):
-        return login_test2()["user_id"]
+    def resourceId02(self):
+        # test2本地资源ID
+        return add_resource01()["resourceId02"]
 
     @property
-    def resourceId(self):
-        return add_resource()["resourId"]
+    def resourceFusionId02(self):
+        # test2联邦资源ID
+        return add_resource02()["resourceFusionId02"]
 
     @property
     def projectId(self):
-        # 联邦资源id
+        # 联邦项目id
         return add_project()['projectId']
 
     @property
     def pid(self):
-        # 联邦资源id
+        # 本地项目id
         return add_project()['pid']
 
 
 
+# def getkey_test1():
+#     key_url = "http://test1.primihub.com/prod-api/sys/common/getValidatePublicKey?timestamp=" + str(
+#             int(time.time())) + "&nonce=" + str(random.randint(0, 9))
+#     result = requests.get(key_url).json()["result"]
+#     publicKey = result["publicKey"]
+#     publicKeyName = result["publicKeyName"]
+#     pubKey = rsa.PublicKey.load_pkcs1_openssl_pem(key_str.format(publicKey).encode())
+#     passwd = Handler.yaml['user01']["userPassword"]
+#     cryptedMessage = rsa.encrypt(passwd.encode(encoding="utf-8"), pubKey)
+#     key_str_text = base64.b64encode(cryptedMessage)
+#
+#     return publicKeyName, key_str_text
 
-def getkey_test1():
-    key_url = "http://test1.primihub.com/prod-api/sys/common/getValidatePublicKey?timestamp=" + str(
-            int(time.time())) + "&nonce=" + str(random.randint(0, 9))
-    result = requests.get(key_url).json()["result"]
-    publicKey = result["publicKey"]
-    publicKeyName = result["publicKeyName"]
-    pubKey = rsa.PublicKey.load_pkcs1_openssl_pem(key_str.format(publicKey).encode())
-    passwd = Handler.yaml['user']["userPassword"]
-    cryptedMessage = rsa.encrypt(passwd.encode(encoding="utf-8"), pubKey)
-    key_str_text = base64.b64encode(cryptedMessage)
+# def login_test1():
+#     """登录测试账号"""
+#     publicKeyName, key_str_text = getkey_test1()
+#     data1 = {"userAccount":Handler.yaml['user01']["userAccount"],
+#              "validateKeyName":publicKeyName,
+#              "timestamp":str(int(time.time())),
+#              "nonce":str(random.randint(0, 9)),
+#              "userPassword":key_str_text}
+#     res = requests_handler.visit(
+#         url=Handler.yaml["host1"]+"/sys/user/login",
+#         method="post",
+#         data=data1
+#     )
+#     # 提取token、
+#     token_test1 = res['result']['token']
+#     user_id_test1 = res["result"]["sysUser"]["userId"]
+#
+#     return {"token":token_test1,"user_id":user_id_test1}
+#
+#
+# def getkey_test2():
+#     key_url = "http://test2.primihub.com/prod-api/sys/common/getValidatePublicKey?timestamp=" + str(
+#             int(time.time())) + "&nonce=" + str(random.randint(0, 9))
+#     result = requests.get(key_url).json()["result"]
+#     publicKey = result["publicKey"]
+#     publicKeyName = result["publicKeyName"]
+#     pubKey = rsa.PublicKey.load_pkcs1_openssl_pem(key_str.format(publicKey).encode())
+#     passwd = Handler.yaml['user02']["userPassword"]
+#     cryptedMessage = rsa.encrypt(passwd.encode(encoding="utf-8"), pubKey)
+#     key_str_text = base64.b64encode(cryptedMessage)
+#
+#     return publicKeyName, key_str_text
+#
+# def login_test2():
+#     """登录测试账号"""
+#     publicKeyName, key_str_text = getkey_test2()
+#     data = {"userAccount":Handler.yaml['user02']["userAccount"],
+#              "validateKeyName":publicKeyName,
+#              "timestamp":str(int(time.time())),
+#              "nonce":str(random.randint(0, 9)),
+#              "userPassword":key_str_text}
+#     res = requests_handler.visit(
+#         url=Handler.yaml["host3"]+"/sys/user/login",
+#         method="post",
+#         data=data
+#     )
+#     # 提取token、
+#     token_test2 = res['result']['token']
+#     user_id_test2 = res["result"]["sysUser"]["userId"]
+#
+#     return {"token":token_test2,"user_id":user_id_test2}
 
-    return publicKeyName, key_str_text
-
-def login_test1():
-    """登录测试账号"""
-    publicKeyName, key_str_text = getkey_test1()
-    data1 = {"userAccount":Handler.yaml['user']["userAccount"],
-             "validateKeyName":publicKeyName,
-             "timestamp":str(int(time.time())),
-             "nonce":str(random.randint(0, 9)),
-             "userPassword":key_str_text}
-    res = requests_handler.visit(
-        url=Handler.yaml["host1"]+"/sys/user/login",
-        method="post",
-        data=data1
-    )
-    # 提取token、
-    token_test1 = res['result']['token']
-    user_id_test1 = res["result"]["sysUser"]["userId"]
-
-    return {"token":token_test1,"user_id":user_id_test1}
-
-
-def getkey_test2():
-    key_url = "http://test2.primihub.com/prod-api/sys/common/getValidatePublicKey?timestamp=" + str(
-            int(time.time())) + "&nonce=" + str(random.randint(0, 9))
-    result = requests.get(key_url).json()["result"]
-    publicKey = result["publicKey"]
-    publicKeyName = result["publicKeyName"]
-    pubKey = rsa.PublicKey.load_pkcs1_openssl_pem(key_str.format(publicKey).encode())
-    passwd = Handler.yaml['user']["userPassword"]
-    cryptedMessage = rsa.encrypt(passwd.encode(encoding="utf-8"), pubKey)
-    key_str_text = base64.b64encode(cryptedMessage)
-
-    return publicKeyName, key_str_text
-
-def login_test2():
-    """登录测试账号"""
-    publicKeyName, key_str_text = getkey_test2()
-    data = {"userAccount":Handler.yaml['user']["userAccount"],
-             "validateKeyName":publicKeyName,
-             "timestamp":str(int(time.time())),
-             "nonce":str(random.randint(0, 9)),
-             "userPassword":key_str_text}
-    res = requests_handler.visit(
-        url=Handler.yaml["host3"]+"/sys/user/login",
-        method="post",
-        data=data
-    )
-    # 提取token、
-    token_test2 = res['result']['token']
-    user_id_test2 = res["result"]["sysUser"]["userId"]
-
-    return {"token":token_test2,"user_id":user_id_test2}
-
-def add_resource():
-    data:str = '{"resourceName":"111","resourceDesc":"111","tags":["111"],"resourceSource":1,"resourceAuthType":1,"fileId":1161,"fieldList":[{"fieldId":null,"fieldName":"Class","fieldType":"integer","fieldDesc":null,"relevance":0,"grouping":0,"protectionStatus":0},{"fieldId":null,"fieldName":"x0","fieldType":"integer","fieldDesc":null,"relevance":0,"grouping":0,"protectionStatus":0},{"fieldId":null,"fieldName":"x1","fieldType":"integer","fieldDesc":null,"relevance":0,"grouping":0,"protectionStatus":0},{"fieldId":null,"fieldName":"x2","fieldType":"integer","fieldDesc":null,"relevance":0,"grouping":0,"protectionStatus":0},{"fieldId":null,"fieldName":"x3","fieldType":"integer","fieldDesc":null,"relevance":0,"grouping":0,"protectionStatus":0},{"fieldId":null,"fieldName":"x4","fieldType":"integer","fieldDesc":null,"relevance":0,"grouping":0,"protectionStatus":0},{"fieldId":null,"fieldName":"x5","fieldType":"integer","fieldDesc":null,"relevance":0,"grouping":0,"protectionStatus":0}],"fusionOrganList":[],"timestamp":"#timestamp#","nonce":802,"token":"#token#"}'
+def add_resource01():
+    # test1环境添加资源
+    data:str = '{"resourceName":"#resourceName#","resourceDesc":"ceasd","tags":["sdf"],"resourceSource":1,"resourceAuthType":1,"fileId":1210,"fieldList":[{"fieldId":null,"fieldName":"Class","fieldType":"integer","fieldDesc":null,"relevance":0,"grouping":0,"protectionStatus":0},{"fieldId":null,"fieldName":"y","fieldType":"integer","fieldDesc":null,"relevance":0,"grouping":0,"protectionStatus":0},{"fieldId":null,"fieldName":"x1","fieldType":"integer","fieldDesc":null,"relevance":0,"grouping":0,"protectionStatus":0},{"fieldId":null,"fieldName":"x2","fieldType":"integer","fieldDesc":null,"relevance":0,"grouping":0,"protectionStatus":0},{"fieldId":null,"fieldName":"x3","fieldType":"integer","fieldDesc":null,"relevance":0,"grouping":0,"protectionStatus":0},{"fieldId":null,"fieldName":"x4","fieldType":"integer","fieldDesc":null,"relevance":0,"grouping":0,"protectionStatus":0},{"fieldId":null,"fieldName":"x5","fieldType":"integer","fieldDesc":null,"relevance":0,"grouping":0,"protectionStatus":0}],"fusionOrganList":[],"timestamp":#timestamp#,"nonce":248,"token":"#token#"}'
     if "#token#" in data:
-        data = data.replace("#token#", Handler().token_test1)
+        data = data.replace("#token#", Handler().yaml["test"]["token"])
     if "#timestamp#" in data:
         data = data.replace("#timestamp#", str(int(time.time())))
+    if "#resourceName#" in data:
+        data = data.replace("#resourceName#", Handler.yaml["test_name"]["resourceName01"])
 
     resp = requests_handler.visit(
         url=Handler.yaml["host2"] + "/resource/saveorupdateresource",
@@ -173,13 +194,47 @@ def add_resource():
         # headers=json.loads(test_info["header"]),
         json=json.loads(data)
     )
-    resourId = resp["result"]["resourceId"]
-    return {"resourId":resourId}
+    resourceId01 = resp["result"]["resourceId"]
+    resourceFusionId01 = resp["result"]["resourceFusionId"]
+    return {"resourceId01":resourceId01, "resourceFusionId01":resourceFusionId01}
+
+def add_resource02():
+    # test2环境添加资源
+    data:str = '{"resourceName":"#resourceName#","resourceDesc":"资源描述","tags":["test"],"resourceSource":1,"resourceAuthType":1,"fileId":1159,"fieldList":[{"fieldId":null,"fieldName":"x6","fieldType":"integer","fieldDesc":null,"relevance":0,"grouping":0,"protectionStatus":0},{"fieldId":null,"fieldName":"x7","fieldType":"integer","fieldDesc":null,"relevance":0,"grouping":0,"protectionStatus":0},{"fieldId":null,"fieldName":"x8","fieldType":"integer","fieldDesc":null,"relevance":0,"grouping":0,"protectionStatus":0},{"fieldId":null,"fieldName":"x9","fieldType":"integer","fieldDesc":null,"relevance":0,"grouping":0,"protectionStatus":0},{"fieldId":null,"fieldName":"x10","fieldType":"integer","fieldDesc":null,"relevance":0,"grouping":0,"protectionStatus":0},{"fieldId":null,"fieldName":"x11","fieldType":"integer","fieldDesc":null,"relevance":0,"grouping":0,"protectionStatus":0},{"fieldId":null,"fieldName":"x12","fieldType":"integer","fieldDesc":null,"relevance":0,"grouping":0,"protectionStatus":0}],"fusionOrganList":[],"timestamp":"#timestamp#","nonce":212,"token":"#token#"}'
+    if "#token#" in data:
+        data = data.replace("#token#", Handler().yaml["test"]["token"])
+    if "#timestamp#" in data:
+        data = data.replace("#timestamp#", str(int(time.time())))
+    if "#resourceName#" in data:
+        data = data.replace("#resourceName#", Handler.yaml["test_name"]["resourceName02"])
+
+    resp = requests_handler.visit(
+        url=Handler.yaml["host4"] + "/resource/saveorupdateresource",
+        method="post",
+        # headers=json.loads(test_info["header"]),
+        json=json.loads(data)
+    )
+    resourceId02 = resp["result"]["resourceId"]
+    resourceFusionId02 = resp["result"]["resourceFusionId"]
+    return {"resourceId02":resourceId02,"resourceFusionId02":resourceFusionId02}
 
 def add_project():
-    data:str = '{"serverAddress":"http://fusion:8080/","projectName":"123","projectDesc":"123","projectOrgans":[{"organId":"2cad8338-2e8c-4768-904d-2b598a7e3298","participationIdentity":2,"resourceIds":["2b598a7e3298-599fa154-1e60-4ce2-aea5-092270bf9dd3"]},{"organId":"8bf56ee6-b004-4ada-b078-591acb22b324","participationIdentity":1,"resourceIds":["591acb22b324-82f7821e-900c-40c1-899f-c481fda8cd5e"]}],"timestamp":#timestamp#,"nonce":622,"token":"#token#"}'
+    # 使用test1和test2的新增资源，在test1环境新建项目
+    data:str = '{"serverAddress":"#serverAddress#","projectName":"#projectName01#","projectDesc":"new_project","projectOrgans":[{"organId":"#organId02#","participationIdentity":2,"resourceIds":["#resourceFusionId02#"]},{"organId":"#organId01#","participationIdentity":1,"resourceIds":["#resourceFusionId01#"]}],"timestamp":#timestamp#,"nonce":622,"token":"#token#"}'
     if "#token#" in data:
-        data = data.replace("#token#", Handler().token_test1)
+        data = data.replace("#token#", Handler().yaml["test"]["token"])
+    if "#serverAddress#" in data:
+        data = data.replace("#serverAddress#", Handler().yaml["test"]["serverAddress"])
+    if "#projectName01#" in data:
+        data = data.replace("#projectName01#", Handler().yaml["test_name"]["projectName01"])
+    if "#organId01#" in data:
+        data = data.replace("#organId01#", Handler().yaml["test"]["organId01"])
+    if "#organId02#" in data:
+        data = data.replace("#organId02#", Handler().yaml["test"]["organId02"])
+    if "#resourceFusionId01#" in data:
+        data = data.replace("#resourceFusionId01#", str(Handler.resourceFusionId01))
+    if "#resourceFusionId02#" in data:
+        data = data.replace("#resourceFusionId02#", str(Handler.resourceFusionId02))
     if "#timestamp#" in data:
         data = data.replace("#timestamp#", str(int(time.time())))
     resp = requests_handler.visit(
@@ -188,10 +243,30 @@ def add_project():
         # headers=json.loads(test_info["header"]),
         json=json.loads(data)
     )
+
     projectId = resp['result']['projectId']
     pid = resp['result']['id']
     return {"projectId":projectId,"pid":pid}
 
+def getProjectDetails():
+    # test2请求项目详情，获得项目资源真实ID，以便进行项目审核、资源审核
+    data = '{"id":"#projectId#","timestamp":#timestamp#,"nonce":622,"token":"#token#"}'
+    if "#timestamp#" in data:
+        data = data.replace("#timestamp#", str(int(time.time())))
+    if "#projectId#" in data:
+        data = data.replace("#projectId#", str(Handler().projectId))
+    if "#token#" in data:
+        data = data.replace("#token#", Handler().yaml["test"]["token"])
+    print(data)
+
+    resp = requests_handler.visit(
+        url=Handler.yaml["host4"] + "/project/getProjectDetails",
+        method="get",
+        # headers=json.loads(test_info["header"]),
+        params=json.loads(data)
+    )
+    #
+    return resp
 
 
 if __name__ == "__main__":
@@ -203,14 +278,17 @@ if __name__ == "__main__":
     # # data = db.query("select * from privacy_test1.data_resource where user_id= 4;",one=False)
     # data = db.query("select * from privacy_test1.data_resource where user_id={}".format(Handler().user_id), one=False)
     # print(data)
-    print(Handler().token_test1)
-    print(Handler().projectId)
-    print(Handler().id)
+    # print(Handler().token_test1)
+    # print(Handler().projectId)
+    #print(Handler().resourceFusionId01)
+    #print(Handler().resourceFusionId02)
     # print(add_project())
+    print(getProjectDetails())
     # print(add_resource())
 
-    # 测试登录函数
+    # 测试登录函数，由于登录加入滑动图片验证码，且错误率高，则使用万能token
     # print(login())
     # print(Handler().token_test1)
+    # print(Handler().token_test2)
 
 
--- testcases/test_project_approval.py
+++ testcases/test_project_approval.py
@@ -4,58 +4,50 @@ import unittest
 import ddt
 
 from common import requests_handler, yaml_handler
-from middleware.handler import Handler
+from middleware.handler import Handler,add_project
 
 # 初始化数据
 logger = Handler.logger
 env_data = Handler()
-test_data = Handler.excel.read_data("resource")
+test_data = Handler.excel.read_data("project_approval")
 
 @ddt.ddt
 class TestResource(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls) -> None:
-        # cls.token = yaml["test"]["token"]
-        # cls.userid = yaml["test"]["userid"]
-        cls.resourceId = env_data.resourceId01
+        cls.project = add_project()
 
     def setUp(self) -> None:
-        self.db = env_data.db_class()
+        pass
+        #self.db = env_data.db_class()
 
     def tearDown(self) -> None:
-        self.db.close()
+        pass
+        #self.db.close()
 
     @ddt.data(*test_data)
     def test_resource(self,test_info):
 
         data = test_info["data"]
         if "#token#" in data:
-            data = data.replace("#token#",env_data.yaml["test"]["token"])
-
+            data = data.replace("#token#", env_data.yaml["test"]["token"])
         if "#timestamp#" in data:
-            data = data.replace("#timestamp#",str(int(time.time())))
-
-        if "#resourceId#" in data:
-            data = data.replace("#resourceId#", str(self.resourceId))
-
-        # if test_info["sql_code"]:
-        #     before_resource = self.db.query(
-        #         "select * from privacy_test1.data_resource where user_id={}".format(self.userid),
-        #         one=False
-        #     )
-        #     print(len(before_resource))
+            data = data.replace("#timestamp#", str(int(time.time())))
+        if "#pid#" in data:
+            data = data.replace("#pid#", str(self.project["pid"]))
 
 
         actual = requests_handler.visit(
             url=env_data.yaml["host2"] + test_info["url"],
             method=test_info["method"],
-            #headers=json.loads(test_info["header"]),
-            json=json.loads(data)
+            # headers=json.loads(test_info["header"]),
+            data=json.loads(data)
         )
         print(actual)
         expected_dict = eval(test_info["expected"])
-        # 断言：code和msg
+        print(expected_dict)
+        # # 断言：code和msg
         try:
             for key, value in expected_dict.items():
                 self.assertTrue(value == actual[key])
@@ -63,7 +55,6 @@ class TestResource(unittest.TestCase):
         except AssertionError as e:
             logger.error("{}：测试用例无法通过：{}".format(test_info["title"],e))
             raise e
-
 
 
 if __name__ == '__main__':
